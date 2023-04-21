6c80e23417d9a25d9319fbbe4cfb49c8437896f7
jef53 <mottes963@gmail.com>
2023-04-21 05:45:14 -0300

    initiating login things

---
+++ src/pages/login.tsx
@@ -0,0 +1,17 @@
+import { LoginContainer, LogoSection, WelcomeSection } from "@/styles/pages/login";
+
+export default function Login() {
+  return (
+    <LoginContainer>
+      <LogoSection>
+        <img src="/images/logo.svg" alt="Logo" />
+      </LogoSection>
+
+      <WelcomeSection>
+
+      </WelcomeSection>
+
+
+    </LoginContainer>
+  )
+}
