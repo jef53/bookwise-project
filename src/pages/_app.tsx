a0776d7d074a6956f763a3ac5e1ddb097cac293a
jef53 <mottes963@gmail.com>
2023-04-21 05:45:44 -0300

    login stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app';
 import { Nunito } from '@next/font/google';
+import { globalStyles } from '@/styles/global';
 
 export const nunito = Nunito({ subsets: ['latin'] })
+
+globalStyles();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div
-      className={`${nunito.className}`}
+      className={nunito.className}
     >
       <Component {...pageProps} />
     </div>
