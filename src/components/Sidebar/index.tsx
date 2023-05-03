f8c116027e28d8a0468ab6dc7a9a801032ae6b40
jef53 <mottes963@gmail.com>
2023-05-03 15:12:44 -0300

    finished sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,13 +1,17 @@
 import { Binoculars, ChartLineUp, User } from "@phosphor-icons/react";
+import { signOut, useSession } from "next-auth/react";
 import Link, { LinkProps } from "next/link"
-import { useRouter } from "next/router"
+import { useRouter, } from "next/router"
 import styles from "./styles.module.scss"
 
 
 export const Sidebar = () => {
   const router = useRouter();
-  return (
 
+  const { data: session } = useSession();
+  const user = session?.user;
+
+  return (
     <div className={styles.Container}>
       <div className={styles.block}>
         <img src="/images/logo.svg" alt="logo" className={styles.logo} />
@@ -16,8 +20,10 @@ export const Sidebar = () => {
         <Link href='/'><User size={24} /> Perfil</Link>
       </div>
 
-      <div className={styles.loginAndLogout}>Fazer login <img src='/images/sidebar/login.svg' /></div>
-    </div>
+      {user ?
+        (<div onClick={() => { signOut() }} className={styles.loginAndLogout}><img src={user?.avatar_url} />{user?.name}<img className={styles.sessionIcon} src='/images/sidebar/logout.svg' /></div>)
+        : (<div onClick={() => { router.push('/login') }} className={styles.loginAndLogout}>Fazer login  <img src='/images/sidebar/login.svg' className={styles.sessionIcon} /></div>)}
 
+    </div>
   )
 }
